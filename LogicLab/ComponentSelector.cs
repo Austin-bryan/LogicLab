9f85d2b5e553acb070c32b30a1a18e0bc0d52dad
Austin Bryan <w.austin.bryan@gmail.com>
2024-03-13 21:29:52 -0400

    Group drag

---
+++ LogicLab/ComponentSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using System.Windows;
+
+namespace LogicLab;
+
+public static class ComponentSelector
+{
+    private static readonly List<LogicComponent> selectedComponents = [];
+    public static ImmutableList<LogicComponent> SelectedComponents => selectedComponents.ToImmutableList();
+
+    public static void SingleSelect(LogicComponent logicComponent)
+    {
+        DeselectAll(logicComponent);
+        selectedComponents.Add(logicComponent);
+    }
+    public static void Deselect(LogicComponent logicComponent)
+    {
+        selectedComponents.Remove(logicComponent);
+    }
+    public static bool IsSelected(LogicComponent logicComponent) => selectedComponents.Contains(logicComponent);
+    public static void DeselectAll(LogicComponent? ignore = null)
+    {
+        for (int i = selectedComponents.Count - 1; i >= 0; i--)
+            if (selectedComponents[i] != ignore)
+                selectedComponents[i].Deselect();
+    }
+
+    public static void ShiftSelect(LogicComponent logicComponent)
+    {
+        selectedComponents.Add(logicComponent);
+    }
+}
