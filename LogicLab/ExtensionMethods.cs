e7c3d6c299ebddffce9b18524f4835e2b72898ca
Austin Bryan <w.austin.bryan@gmail.com>
2024-03-15 12:38:49 -0400

    Added missing files

---
+++ LogicLab/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LogicLab;
+
+public static class ExtensionMethods
+{
+    public static MainWindow MainWindow(this UserControl userControl)
+    {
+        return Window.GetWindow(userControl) as MainWindow ?? throw new NullReferenceException("Null ref");
+    }
+
+    public static Label DebugLabel(this UserControl userControl) => userControl.MainWindow().DebugLabel;
+    public static Grid MainGrid(this UserControl userControl) => userControl.MainWindow().MainGrid;
+}
---
+++ LogicLab/Wire.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LogicLab;
+
+public partial class Wire : LogicComponent
+{
+    private readonly Path splinePath = new()
+    {
+        Stroke = new SolidColorBrush(Color.FromRgb(200, 200, 200)),
+        StrokeThickness = 8
+    };
+    public Wire(MainWindow mainWindow, Point startPoint)
+    {
+        Dragger = null;
+        mainWindow.MainGrid.Children.Insert(0, splinePath);
+
+        this.startPoint = startPoint;
+        splinePath.MouseDown += Spline_MouseDown;
+        splinePath.MouseUp += Gate_MouseUp;
+        splinePath.Effect = Effect;
+    }
+
+    private Point startPoint;
+
+    public void Draw(Point end)
+    {
+        const int offset = 25;
+
+        Point start = new Point(startPoint.X, startPoint.Y - offset);
+        end = new Point(end.X, end.Y - offset);
+
+        PathGeometry pathGeometry = new();
+        PathFigure pathFigure = new()
+        {
+            StartPoint = start
+        };
+
+        BezierSegment bezierSegment = new(
+            new Point(start.X + 50, start.Y), // Control point 1
+            new Point(end.X - 50, end.Y),     // Control point 2
+            end,                              // End point
+            isStroked: true
+        );
+
+        pathFigure.Segments.Add(bezierSegment);
+        pathGeometry.Figures.Add(pathFigure);
+        splinePath.Data = pathGeometry;
+    }
+    private void Spline_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        Gate_MouseDown(sender, e);
+        splinePath.Effect = Effect;
+    }
+}
