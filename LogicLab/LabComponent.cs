07e8a4d7d035a54b41c5091194c7f4c5f55ab046
Austin Bryan <w.austin.bryan@gmail.com>
2024-03-27 12:36:46 -0400

    Remade output toggle

---
+++ LogicLab/LabComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Effects;
+using System.Windows.Media;
+
+namespace LogicLab;
+
+public abstract class LabComponent : UserControl
+{
+    protected static bool ShiftKey => Keyboard.Modifiers.HasFlag(ModifierKeys.Shift);
+    protected readonly DropShadowEffect shadow    = new() { ShadowDepth = 4, Color = Colors.Black, Opacity = 0.8, BlurRadius = 6 };
+    protected readonly DropShadowEffect highlight = new() { ShadowDepth = 0, Color = Color.FromRgb(200, 200, 255), Opacity = 1, BlurRadius = 10 };
+    protected DropShadowEffect animatingShadow;
+
+    public LabComponent() => animatingShadow = shadow;
+    public abstract void ShowSignal(bool? signal);
+}
