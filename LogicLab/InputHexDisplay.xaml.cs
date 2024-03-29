f3888f7d6c66983edfc9c6282a26742be248c00b
merfman <garyarmstrong100@gmail.com>
2024-03-29 10:16:28 -0400

    - added InputHexDisplay.xaml     - so far its just a copy of inputPixel

---
+++ LogicLab/InputHexDisplay.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogicLab;
+public partial class InputHexDisplay
+{
+    protected override Grid ComponentGrid => Grid;
+    protected override Rectangle BackgroundRect => BackgroundSprite;
+    public InputHexDisplay() => InitializeComponent();
+    protected override void Grid_Loaded(object sender, RoutedEventArgs e) => OnLoaded();
+
+    public override void ShowSignal(bool? signal)
+    {
+        Color targetColor = signal == true ? Color.FromRgb(150, 150, 30) : Color.FromRgb(30, 30, 30);
+        BackgroundSprite.Fill.BeginAnimation(SolidColorBrush.ColorProperty, new ColorAnimation(targetColor, TimeSpan.FromSeconds(0.25)));
+    }
+}
