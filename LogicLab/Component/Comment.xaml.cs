5a6e395260737bba2e81bc764dd42e9e0e2ce765
Steven <steve.burke@snhu.edu>
2024-04-15 04:56:55 -0400

    Revereted to a working state & planned out future updates

--- LogicLab/Component/Comment.xaml.cs
+++ LogicLab/Component/Comment.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -16,10 +17,13 @@ public partial class Comment : UserControl
 
     private void Rectangle_MouseMove(object sender, MouseEventArgs e)
     {
+
         const int titleBarLength = 40;
         const int sideBarLength = 10;
         const int cornerLength = 30;
         var pos = e.GetPosition(this);
+        bool isMouseDown;
+        
 
         this.MainWindow().DebugLabel.Content += CommentBox.ActualHeight.ToString() + ", ";
         //this.MainWindow().DebugLabel.Content += e.GetPosition(this).ToString() + ", ";
@@ -29,67 +33,93 @@ public partial class Comment : UserControl
         {
           //  System.Drawing.Cursor cursor = System.Windows.Forms.Cursors.NE;
             Cursor = Cursors.ScrollNW;
+            //move the draging code into each bar's conditional as an if MouseDown Event
+
+
+
+
+            //make sure to ask austin how to use the MouseDown Event, How to fix resizing to be only in the dragged direction and how to make ActualHeight edditable
+
+
+
+
         }
         // Top Right Corner
         else if (pos.X > CommentSprite.ActualWidth - cornerLength && pos.Y < cornerLength)
         {
             Cursor = Cursors.ScrollNE;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
         // Bottom Left Corner
         else if (pos.X < cornerLength && pos.Y > CommentSprite.ActualHeight - cornerLength)
         {
             Cursor = Cursors.ScrollSW;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
         // Bottom Right Corner
         else if (pos.X > CommentSprite.ActualWidth - cornerLength && pos.Y > CommentSprite.ActualHeight - cornerLength)
         {
             Cursor = Cursors.ScrollSE;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
 
         // Top Bar
         else if (pos.Y < sideBarLength-5)
         {
             Cursor = Cursors.SizeNS;
+            //move the draging code into each bar's conditional as an if MouseDown Event
 
         }
         else if (pos.Y < titleBarLength)
         {
             Cursor = Cursors.SizeAll;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
         // Left Bar
         else if (pos.X < sideBarLength)
         {
             Cursor = Cursors.SizeWE;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
         // Right Bar
         else if (pos.X > CommentSprite.ActualWidth - sideBarLength)
         {
             Cursor = Cursors.SizeWE;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
         // Bottom Bar
         else if (pos.Y > CommentSprite.ActualHeight - sideBarLength)
         {
             Cursor = Cursors.SizeNS;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
         else
         {
             Cursor = Cursors.Arrow;
+            //move the draging code into each bar's conditional as an if MouseDown Event
         }
+        //void DragStart(MouseEventArgs e)
+        //{
+
+        //isMouseDown = true;
+
+
+        //}
 
         // making height change depending on cursor type
-        if (Cursor == Cursors.SizeNS)
-        {
-            ActualHeight = ActualHeight + ;
-        }
-        else if (Cursor == Cursors.SizeWE)
-        {
-            ActualWidth = ActualWidth + ;
-        }
-        else if (Cursor == Cursors.ScrollNW)
-        {
-            ActualHeight = ActualHeight + ;
-            ActualWidth = ActualWidth + ;
-        }
+        //if (Cursor == Cursors.SizeNS)
+        //{
+        //ActualHeight = ActualHeight + ;
+        //}
+        //else if (Cursor == Cursors.SizeWE)
+        //{
+        //  ActualWidth = ActualWidth + ;
+        //}
+        //else if (Cursor == Cursors.ScrollNW && isMouseDown == true)
+        //{
+        // ActualHeight = ActualHeight + ;
+        // ActualWidth = ActualWidth + ;
+        //}
     }
 
     //move and drag the comments different places
